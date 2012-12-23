cc05acfa2986134c9451d103bbb4f25820f9d42f
Adrian Mateoaea <adrianitech@outlook.com>
2012-12-23 10:52:53 +0200

    Images

--- Dl.FM/Dl.FM/View/MainPage.xaml.cs
+++ Dl.FM/Dl.FM/View/MainPage.xaml.cs
@@ -12,45 +12,33 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
-
 namespace Dl.FM.View
 {
-    /// <summary>
-    /// A basic page that provides characteristics common to most applications.
-    /// </summary>
     public sealed partial class MainPage : Dl.FM.Common.LayoutAwarePage
     {
         public MainPage()
         {
             this.InitializeComponent();
-
-            var list = new List<string>();
-            for (int i = 0; i < 10; i++) list.Add(i.ToString());
-            
         }
 
-        /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
-        /// </summary>
-        /// <param name="navigationParameter">The parameter value passed to
-        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
-        /// </param>
-        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
-        /// session.  This will be null the first time a page is visited.</param>
         protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
         {
         }
 
-        /// <summary>
-        /// Preserves state associated with this page in case the application is suspended or the
-        /// page is discarded from the navigation cache.  Values must conform to the serialization
-        /// requirements of <see cref="SuspensionManager.SessionState"/>.
-        /// </summary>
-        /// <param name="pageState">An empty dictionary to be populated with serializable state.</param>
         protected override void SaveState(Dictionary<String, Object> pageState)
         {
+        }
+
+        private void SemanticZoom_ViewChangeStarted(object sender, SemanticZoomViewChangedEventArgs e)
+        {
+            if (!e.IsSourceZoomedInView)
+            {
+                var sz = sender as SemanticZoom;
+                var zil = sz.ZoomedInView as GridView;
+                var zol = sz.ZoomedOutView as GridView;
+                var index = zol.Items.IndexOf(e.SourceItem.Item);
+                e.DestinationItem.Item = zil.Items[index];
+            }
         }
     }
 }
