8d68289c4ee4664bfd82c6af78269cbeb7717ffe
Faisal <f7000r@hotmail.com>
2014-07-03 23:52:52 +0300

    Improve DurationConverter

--- DI.FM/DI.FM.Shared/Converters/DurationConverter.cs
+++ DI.FM/DI.FM.Shared/Converters/DurationConverter.cs
@@ -8,22 +8,11 @@ namespace DI.FM.Common
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             var duration = double.Parse(value.ToString());
-            if (duration < 0) duration = 0;
 
-            var time = TimeSpan.FromSeconds(duration);
+            if (duration < 0)
+                duration = 0;
 
-            string min = time.Minutes.ToString();
-            string sec = time.Seconds.ToString();
-
-            if (min.Length == 1) min = "0" + min;
-            if (sec.Length == 1) sec = "0" + sec;
-
-            if (time.Hours == 0) return string.Format("{0}:{1}", min, sec);
-
-            string hours = time.Hours.ToString();
-            if (hours.Length == 1) hours = "0" + hours;
-
-            return string.Format("{0}:{1}:{2}", hours, min, sec);
+            return TimeSpan.FromSeconds(duration).ToString(@"hh\:mm\:ss");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
